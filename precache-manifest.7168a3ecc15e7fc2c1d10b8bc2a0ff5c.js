d66cbea6538744f35c27c839c5bb43639c47e232
spandanmehta <spandan33@gmail.com>
2019-11-06 00:39:00 +0530

    ok

---
+++ precache-manifest.7168a3ecc15e7fc2c1d10b8bc2a0ff5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45e7588ed3043f6add5b9bcae255a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "736b049c780186f6b9b8",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "9cabfcbd8b2b087fdd90",
+    "url": "/static/js/2.953b2a08.chunk.js"
+  },
+  {
+    "revision": "736b049c780186f6b9b8",
+    "url": "/static/js/main.8de006ff.chunk.js"
+  },
+  {
+    "revision": "dacfdc5213d798bee0ed",
+    "url": "/static/js/runtime~main.4006f548.js"
+  }
+]);
